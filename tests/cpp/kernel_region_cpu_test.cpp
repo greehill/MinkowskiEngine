d8c554212d2f7c5e1808f1843cc073ca8d4f280a
Benjamin Berta <beni9512@gmail.com>
2026-08-01 04:48:55 +0200

    Return bindable maps from C++ kernel test

--- tests/cpp/kernel_region_cpu_test.cpp
+++ tests/cpp/kernel_region_cpu_test.cpp
@@ -88,7 +88,7 @@ region_iterator_test(const torch::Tensor &coordinates,
   return all_regions;
 }
 
-std::tuple<cpu_kernel_map, size_type, double>
+std::tuple<std::pair<cpu_in_maps, cpu_out_maps>, size_type, double>
 kernel_map_test(const torch::Tensor &in_coordinates,
                 const torch::Tensor &out_coordinates,
                 const torch::Tensor &kernel_size) {
@@ -127,14 +127,8 @@ kernel_map_test(const torch::Tensor &in_coordinates,
   CoordinateMapCPU<coordinate_type> in_map{N_in, D};
   CoordinateMapCPU<coordinate_type> out_map{N_out, D};
 
-  auto in_coordinate_range = coordinate_range<coordinate_type>(N_in, D, ptr);
-  simple_range iter_in{N_in};
-  in_map.insert(ptr,
-                ptr + N_in * D);
+  in_map.insert(ptr, ptr + N_in * D);
 
-  auto out_coordinate_range =
-      coordinate_range<coordinate_type>(N_out, D, ptr_out);
-  simple_range iter_out{N_out};
   out_map.insert(ptr_out, ptr_out + N_out * D);
 
   LOG_DEBUG("coordinate initialization");
@@ -159,7 +153,9 @@ kernel_map_test(const torch::Tensor &in_coordinates,
   t.tic();
   auto result = in_map.kernel_map(out_map, region);
 
-  return std::make_tuple(result, out_map.size(), t.toc());
+  return std::make_tuple(
+      std::make_pair(std::move(result.first), std::move(result.second)),
+      out_map.size(), t.toc());
 }
 
 } // namespace minkowski
